deecf518b5e85721171cd44711da76e1528c7c34
JP Dillingham <jpdillingham@gmail.com>
2018-04-14 11:21:59 -0500

    add subheaders

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import { List, ListItem } from 'material-ui/List';
+import Subheader from 'material-ui/Subheader'
 import CheckBox from 'material-ui/svg-icons/toggle/check-box'
 import CheckBoxOutlineBlank from 'material-ui/svg-icons/toggle/check-box-outline-blank'
 import TextField from 'material-ui/TextField'
@@ -51,8 +52,8 @@ class App extends Component {
             disabled={this.state.newTodo === ''}
             onClick={this.handleAdd}
           />
-          <span>To Do</span>
           <List>
+            <Subheader>Active</Subheader>
             {this.state.todos
               .filter(todo => !todo.done)
               .map(todo => 
@@ -61,17 +62,15 @@ class App extends Component {
                 >{todo.todo}</ListItem>
               )
             }
-          </List>
-          <span>Done</span>
-          <List>
+            {this.state.todos.filter(todo => todo.done).length > 0 ? 
+            <Subheader>Done</Subheader> : ''}
             {this.state.todos
               .filter(todo => todo.done)
               .map(todo => 
                 <ListItem
                   leftIcon={<CheckBox/>}
                 >{todo.todo}</ListItem>
-              )
-            }
+              )}
           </List>
         </div>
       </MuiThemeProvider>
