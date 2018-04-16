38a40ef7c897a8fa276d9fedbad0aec0b1aa50bc
JP Dillingham <jpdillingham@gmail.com>
2018-04-16 18:36:58 -0500

    add local and remote api urls

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
-export const API_URL = "http://localhost:3001/todos";
+export const LOCAL_API_URL = "http://localhost:3001/todos";
+export const REMOTE_API_URL = "https://z6jg3z37xi.execute-api.us-east-1.amazonaws.com/prod/todos"
