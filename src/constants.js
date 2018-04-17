1334ef807031236b0a3ba3db7614e7e922de26fe
JP Dillingham <jpdillingham@gmail.com>
2018-04-16 21:40:29 -0500

    Update url

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
 export const LOCAL_API_URL = "http://localhost:3001/todos";
-export const REMOTE_API_URL = "https://z6jg3z37xi.execute-api.us-east-1.amazonaws.com/prod/todos"
+export const REMOTE_API_URL = "https://5o2vop75a9.execute-api.us-east-1.amazonaws.com/prod/todos"
