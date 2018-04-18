f8eaee4d8469d1cad1175d539e89cf907e8d743c
JP Dillingham <jpdillingham@gmail.com>
2018-04-17 21:30:15 -0500

    add dynamo integration

--- src/server.js
+++ src/server.js
@@ -11,32 +11,63 @@ app.use(cors());
 app.use(bodyParser.json());  
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const database = require('./database');
+const user = 'default-user';
+
 let todos = [];
 
 app.get('/todos', (req, res) => {  
-    res.status(200);
-    res.json(todos);
+    database.get('todos', user)
+    .then(data => {
+        let todos = data && data.Item && data.Item.todos ? data.Item.todos : [];
+
+        res.status(200);
+        res.json(todos);
+    })
 });
 
 app.post('/todos', (req, res) => {
     let todo = req.body;
 
-    todos.push(todo);
+    database.get('todos', user)
+    .then(data => {
+        let todos = data && data.Item && data.Item.todos ? data.Item.todos : [];
 
-    res.status(200);
-    res.json(todos);
+        todos.push(todo);
+        return todos;
+    })
+    .then(todos => {
+        database.set('todos', user, todos);
+        return todos;
+    })
+    .then(todos => {
+        res.status(200);
+        res.json(todos);
+    });
 });
 
 app.put('/todos/:id', (req, res) => {
     let id = req.params.id;
     let todo = req.body;
 
-    todos = todos.map(t => { 
-        return t.id === id ? todo : t
-    });
+    database.get('todos', user)
+    .then(data => {
+        let todos = data && data.Item && data.Item.todos ? data.Item.todos : [];
 
-    res.status(200);
-    res.json(todos);
+        todos = todos.map(t => { 
+            return t.id === id ? todo : t
+        });
+
+        return todos;
+    })
+    .then(todos => {
+        database.set('todos', user, todos);
+        return todos;
+    })
+    .then(todos => {
+        res.status(200);
+        res.json(todos);
+    });
 });
 
 app.listen(port, () => console.log('Listening on port ' + port + '.')); // ignored by lambda
